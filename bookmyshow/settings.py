307b715de3c3d99033b4a20ff7430778397fa21b
Kumkum Kumari <kumkumgupta24.654@gmail.com>
2026-05-22 21:40:50 +0530

    first commit

--- bookmyshow/settings.py
+++ bookmyshow/settings.py
@@ -92,7 +92,7 @@ DATABASES = {
     }
 }
 
-DATABASES ['default'] = dj_database_url.parse('postgresql://django_bookmyshow_wiys_user:aGbXHcLvDuqbqI0zdS7fOIteU6NRzGbB@dpg-d83b1ijtqb8s73dhel10-a.virginia-postgres.render.com/django_bookmyshow_wiys')
+DATABASES['default'] = dj_database_url.parse('postgresql://django_bookmyshow_wiys_user:aGbXHcLvDuqbqI0zdS7fOIteU6NRzGbB@dpg-d83b1ijtqb8s73dhel10-a.virginia-postgres.render.com/django_bookmyshow_wiys')
 
 
 
